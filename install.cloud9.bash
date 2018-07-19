bf8449770b0632529bac32c6166a16da35975b93
Katz Ueno <katz@concrete5.co.jp>
2018-07-19 16:09:28 +0900

    8.4.1

--- install.cloud9.bash
+++ install.cloud9.bash
@@ -14,8 +14,8 @@
 # VARIABLES
 # ----------
 
-CONCRETE5_DOWNLOAD="https://www.concrete5.org/download_file/-/view/104344/"
-CONCRETE5_VERSION="concrete5-8.4.0"
+CONCRETE5_DOWNLOAD="http://www.concrete5.org/download_file/-/view/105022/"
+CONCRETE5_VERSION="concrete5-8.4.1"
 DESTINATION="/home/ubuntu/workspace"
 CONCRETE_PHP_RAW="https://raw.githubusercontent.com/katzueno/cloud9-concrete5-install-script/master/config/cloud9.concrete.php"
 INSTALL_LEMP="no"
